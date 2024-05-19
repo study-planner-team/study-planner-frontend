ca6f547f6a673f7abb87190ba2f9ca7f849e873d
Wojdak <jakub.wojdak11@gmail.com>
2024-05-19 18:55:17 +0200

    SP-8 Users are able to register

--- study-planner/src/pages/RegisterPage.tsx
+++ study-planner/src/pages/RegisterPage.tsx
@@ -1,35 +1,75 @@
-import React from 'react';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap';
+import AuthService, { RegisterRequest } from '../services/AuthService'
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../styles/RegisterPageStyles.css';
+import axios from 'axios';
 
 const RegisterPage: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState<string>('');
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const navigate = useNavigate();
+
+  const handleRegister = async (event: React.FormEvent) => {
+    event.preventDefault();
+    
+    const data: RegisterRequest = { username, password, email }
+
+    try {
+      const response = await AuthService.register(data);
+      setMessage(response.data);
+      setErrors([]);
+    } catch (error) {
+      if (Array.isArray(error)) {
+        setErrors(error.map((err: any) => err.errorMessage));
+      } else {
+        setErrors(["Registration failed."]);
+      }
+    }
+
+  }
+  
   return (
     <>
       <Header />
     <Container className="auth-container mb-3">
       <Row className="justify-content-center align-items-center vh-100">
-        <Col md={6} className="auth-box h-50 bg-white">
+        <Col md={6} className="auth-box h-50 bg-white ">
           <h2 className="text-center mt-2">Rejestracja</h2>
-          <Form>
+          <Form onSubmit={handleRegister}>
             <Form.Group controlId="username">
               <Form.Label>Nazwa użytkownika</Form.Label>
-              <Form.Control type="text" placeholder="Nazwa użytkownika"/>
+              <Form.Control type="text" placeholder="Nazwa użytkownika" value={username} onChange={(e) => setUsername(e.target.value)}/>
             </Form.Group>
             <Form.Group controlId="password">
               <Form.Label>Hasło</Form.Label>
-              <Form.Control type="password" placeholder="Hasło" />
+              <Form.Control type="password" placeholder="Hasło" value={password} onChange={(e) => setPassword(e.target.value)}/>
             </Form.Group>
             <Form.Group controlId="email">
               <Form.Label>Adres email</Form.Label>
-              <Form.Control type="email" placeholder="Adres email" />
+              <Form.Control type="email" placeholder="Adres email" value={email} onChange={(e) => setEmail(e.target.value)}/>
             </Form.Group>
             <Button variant="warning" type="submit" className="w-100 d-block mt-3">Zarejestruj</Button>
             <Button variant="danger" type="button" className="w-100 d-block mt-3">Register with Google</Button>
           </Form>
-        </Col>
+          
+          {message && <p className="text-success mt-3">{message}</p>}
+            {errors.length > 0 && (
+              <div className="mt-3 mb-5">
+                {errors.map((error, index) => (
+                  <p key={index} className="text-danger mb-0">{error}</p>
+                ))}
+              </div>
+            )}
+          </Col>
+
         <Col md={6} className="auth-box auth-box-right h-50 d-flex flex-column justify-content-center align-items-center text-center">
           <h3 className="text-center">Witaj!<br/>Masz już u nas konto?</h3>
           <Link to="/login">
