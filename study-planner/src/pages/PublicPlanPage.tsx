a44d3288d73f3995a69d1621afac7ac2fe5f488a
Zehlan1 <e.nalepka02@gmail.com>
2024-11-10 15:28:48 +0100

    SP-90 Added basic filtering for public plans

--- study-planner/src/pages/PublicPlanPage.tsx
+++ study-planner/src/pages/PublicPlanPage.tsx
@@ -9,7 +9,9 @@ import { useAuthContext } from "../context/useAuthContext";
 
 const PublicPlanPage: React.FC = () => {
   const [publicPlans, setPublicPlans] = useState<any[]>([]);
+  const [filteredPlans, setFilteredPlans] = useState<any[]>([]);
   const { user } = useAuthContext();
+  const inputElement = document.getElementById("plan-search") as HTMLInputElement | null;
 
   useEffect(() => {
     fetchPublicPlans();
@@ -19,6 +21,7 @@ const PublicPlanPage: React.FC = () => {
     try {
       const data = await StudyPlanService.getPublicPlans();
       setPublicPlans(data);
+      setFilteredPlans(data);
     } catch (error) {
       console.error("Couldn't fetch public study plans", error);
     }
@@ -33,6 +36,18 @@ const PublicPlanPage: React.FC = () => {
     }
   };
 
+  function handleFilter(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    const filterTemp = publicPlans.filter(e => e.title.toLowerCase().includes(inputElement.value.toLowerCase()));
+    setFilteredPlans(filterTemp);
+
+    console.log("Current input value:", inputElement.value);
+  }
+
+  if (inputElement) {
+    inputElement.addEventListener("input", handleFilter);
+  }
+
   return (
     <>
       <Header />
@@ -41,11 +56,11 @@ const PublicPlanPage: React.FC = () => {
           <h2 className="text-center">Publiczne Plany Nauki</h2>
         </div>
         <div className="d-flex justify-content-center">
-          <input type="text" id="plan-search" />
+          <input type="text" id="plan-search"/>
         </div>
         <Row>
-          {publicPlans.length > 0 ? (
-            publicPlans.map((plan, index) => (
+          {filteredPlans.length > 0 ? (
+            filteredPlans.map((plan, index) => (
               <Col md={4} key={index} className="mb-4">
                 <Card className="custom-bg">
                   <Card.Body>
